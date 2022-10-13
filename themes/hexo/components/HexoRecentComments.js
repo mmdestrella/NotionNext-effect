694ba9dfe31a815ee4788c5b9be09c331c747787
mmdestrella <115703941+mmdestrella@users.noreply.github.com>
2022-10-13 19:08:33 +0800

    Update HexoRecentComments.js

--- themes/hexo/components/HexoRecentComments.js
+++ themes/hexo/components/HexoRecentComments.js
@@ -31,7 +31,7 @@ const HexoRecentComments = (props) => {
         </div>
 
         {onLoading && <div>Loading...<i className='ml-2 fas fa-spinner animate-spin' /></div>}
-        {!onLoading && comments && comments.length === 0 && <div>No Comments</div>}
+        {!onLoading && comments && comments.length === 0 && <div>{locale.COMMON.NO_COMMENTS}</div>}
         {!onLoading && comments && comments.length > 0 && comments.map((comment) => <div key={comment.objectId} className='pb-2 pl-1'>
             <div className='dark:text-gray-200 text-sm waline-recent-content wl-content' dangerouslySetInnerHTML={{ __html: comment.comment }} />
             <div className='dark:text-gray-400 text-gray-400 font-sans text-sm text-right cursor-pointer hover:text-red-500 hover:underline pt-1 pr-2'><Link href={{ pathname: comment.url, hash: comment.objectId, query: { target: 'comment' } }}><a >-- {comment.nick}</a></Link></div>
